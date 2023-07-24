937edc9f4286639d5040debc7b7747e7276ace3b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-07-24 14:20:18 +0700

    use traits, make functional login/register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Auth;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Invalid credentials', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // dd($user);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken,
+        ]);
+    }
+
+    public function logout()
+    {
+
+    }
+
+}
